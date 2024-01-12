d4b15e5747fbc4d1ef58fbba7027be6d11171f7b
piykumar25 <piykumar05i@gmail.com>
2024-01-13 00:27:59 +0530

    Default Bean assigned because of primary annotation

--- example1/src/main/java/com/example/config/ProjectConfig.java
+++ example1/src/main/java/com/example/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
@@ -14,6 +15,7 @@ public class ProjectConfig {
         return veh;
     }
 
+    @Primary // Due to this annotation spring context will consider in case of confusion due to multiple beans present of smae type
     @Bean(value = "ferrariVehicle")
     Vehicle vehicle1() {
         var veh = new Vehicle();
