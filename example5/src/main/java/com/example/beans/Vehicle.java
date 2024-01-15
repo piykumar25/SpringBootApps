b06db5549b42a2bee32559216227947d240c7e5b
piykumar25 <piykumar05i@gmail.com>
2024-01-15 17:38:22 +0530

    Assignment about beans

---
+++ example5/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+
+import com.example.services.VehicleServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component(value = "vehicleBean")
+public class Vehicle {
+
+    private String name = "Honda";
+    private final VehicleServices vehicleServices;
+
+    @Autowired
+    public Vehicle(VehicleServices vehicleServices) {
+        this.vehicleServices = vehicleServices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public VehicleServices getVehicleServices() {
+        return vehicleServices;
+    }
+}
---
+++ example5/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = {"com.example.implementation",
+"com.example.services"})
+@ComponentScan(basePackageClasses = {com.example.beans.Vehicle.class,
+com.example.beans.Person.class})
+public class ProjectConfig {
+
+
+}
