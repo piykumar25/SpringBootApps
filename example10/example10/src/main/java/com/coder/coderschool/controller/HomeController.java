8b4058489b0e85033f3f84f28619a92bbab47ff4
piykumar25 <piykumar05i@gmail.com>
2024-01-27 19:22:02 +0530

    Baic of Thymeleaf

--- example10/example10/src/main/java/com/coder/coderschool/controller/HomeController.java
+++ example10/example10/src/main/java/com/coder/coderschool/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.coder.coderschool.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage(Model model) {
-        model.addAttribute("username", "Piyush Kumar");
+    public String displayHomePage() {
         return "home.html";
     }
 }
