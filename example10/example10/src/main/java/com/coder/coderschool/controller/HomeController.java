41295f07c65ce96d4078d6c1d83381779ca18070
piykumar25 <piykumar05i@gmail.com>
2024-01-27 18:58:00 +0530

    Dev tools information

--- example10/example10/src/main/java/com/coder/coderschool/controller/HomeController.java
+++ example10/example10/src/main/java/com/coder/coderschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.coder.coderschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Piyush Kumar");
         return "home.html";
     }
 }
