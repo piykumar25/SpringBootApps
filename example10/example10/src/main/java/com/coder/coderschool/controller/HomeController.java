68ee14821df0cdce9e9f597a5e6318d3bf6bb3da
piykumar25 <piykumar05i@gmail.com>
2024-01-27 18:15:34 +0530

    Basic of Spring boot App

---
+++ example10/example10/src/main/java/com/coder/coderschool/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.coder.coderschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage() {
+        return "home.html";
+    }
+}
---
+++ example10/example10/src/test/java/com/coder/coderschool/CoderSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.coderschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
