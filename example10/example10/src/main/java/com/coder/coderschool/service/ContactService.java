0bdf9f4b0cec52eca136106a604e9ebfdfe8733f
piykumar25 <piykumar05i@gmail.com>
2024-01-28 00:14:49 +0530

    Thymeleaf concept

---
+++ example10/example10/src/main/java/com/coder/coderschool/service/ContactService.java
@@ -0,0 +1,24 @@
+package com.coder.coderschool.service;
+
+import com.coder.coderschool.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+}
