d6be12638ccfef80c771a2c98a82e51cfff72ff3
piykumar25 <piykumar05i@gmail.com>
2024-01-28 23:08:28 +0530

    Folder structure change

--- example10/src/main/java/com/coder/coderschool/controller/ContactController.java
+++ example10/src/main/java/com/coder/coderschool/controller/ContactController.java
@@ -2,10 +2,14 @@ package com.coder.coderschool.controller;
 
 import com.coder.coderschool.model.Contact;
 import com.coder.coderschool.service.ContactService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +29,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
@@ -42,9 +47,13 @@ public class ContactController {
 //    }
 
     @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()) {
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 
