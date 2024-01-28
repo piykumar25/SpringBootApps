7ecaf970881b81fb1bc221229265838b5198f839
piykumar25 <piykumar05i@gmail.com>
2024-01-28 13:47:17 +0530

    lombok library

--- example10/example10/src/main/java/com/coder/coderschool/service/ContactService.java
+++ example10/example10/src/main/java/com/coder/coderschool/service/ContactService.java
@@ -1,14 +1,16 @@
 package com.coder.coderschool.service;
 
 import com.coder.coderschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
 
-    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
+//    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
     /**
      * Save Contact Details into DB
      * @param contact
