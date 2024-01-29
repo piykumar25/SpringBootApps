38851f5df5553d773481116ae68833dac84bc664
piykumar25 <piykumar05i@gmail.com>
2024-01-29 14:33:22 +0530

    Application Scope -- Just one bean is created for the application

--- example10/src/main/java/com/coder/coderschool/service/ContactService.java
+++ example10/src/main/java/com/coder/coderschool/service/ContactService.java
@@ -5,13 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
 //@RequestScope
-@SessionScope
+//@SessionScope
+@ApplicationScope
 public class ContactService {
 
 //    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
