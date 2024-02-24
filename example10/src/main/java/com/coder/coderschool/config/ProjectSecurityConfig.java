bda6869699e5f970e42f135350ebbef506e5b85f
piykumar25 <piykumar05i@gmail.com>
2024-02-24 22:45:30 +0530

    permit all demo

---
+++ example10/src/main/java/com/coder/coderschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.coder.coderschool.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // Permit All Requests inside the Web Application
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
