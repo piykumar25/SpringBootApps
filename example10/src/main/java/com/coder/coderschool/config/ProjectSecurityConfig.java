9be5a4b6219a53b76fc0118ee48e255dbe298976
piykumar25 <piykumar05i@gmail.com>
2024-02-24 23:13:17 +0530

    Demo about requestMatchers and CSRF issue

--- example10/src/main/java/com/coder/coderschool/config/ProjectSecurityConfig.java
+++ example10/src/main/java/com/coder/coderschool/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,8 +16,16 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        // Permit All Requests inside the Web Application
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/**","/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
