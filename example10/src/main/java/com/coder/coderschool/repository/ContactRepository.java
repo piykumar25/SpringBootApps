6fad260296de33417b595368f64aac41cc3ddbd2
piykumar25 <piykumar05i@gmail.com>
2024-03-20 19:03:03 +0530

    migrated H2 database to Mysql database

--- example10/src/main/java/com/coder/coderschool/repository/ContactRepository.java
+++ example10/src/main/java/com/coder/coderschool/repository/ContactRepository.java
@@ -24,14 +24,14 @@ public class ContactRepository {
     }
 
     public int saveContactMsg(Contact contact) {
-        String sql = "INSERT INTO CONTACT_MSG (name, mobile_num, email, subject, message, status, created_at, created_by)" +
+        String sql = "INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)" +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         return jdbcTemplate.update(sql, contact.getName(), contact.getMobileNum(), contact.getEmail(), contact.getSubject(), contact.getMessage(),
                 contact.getStatus(), contact.getCreatedAt(), contact.getCreatedBy());
     }
 
     public List<Contact> findMsgsWithStatus(String status) {
-        String sql = "SELECT * FROM CONTACT_MSG WHERE status = ?";
+        String sql = "SELECT * FROM contact_msg WHERE status = ?";
         return jdbcTemplate.query(sql, new PreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps) throws SQLException {
@@ -41,7 +41,7 @@ public class ContactRepository {
     }
 
     public int updateMsgStatus(int contactId, String status, String updatedBy) {
-        String sql = "UPDATE CONTACT_MSG SET status = ?, updated_by = ?, updated_at = ? WHERE contact_id = ?";
+        String sql = "UPDATE contact_msg SET status = ?, updated_by = ?, updated_at = ? WHERE contact_id = ?";
         return jdbcTemplate.update(sql, new PreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps) throws SQLException {
